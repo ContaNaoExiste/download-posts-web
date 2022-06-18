4a663cf5087d608ad794d86c77fe85c23d4efbc3
ContaNaoExiste <ContaNaoExiste@emaildebase0001@gmail.com>
2022-06-18 15:43:51 -0300

    Revert "Commit inicial com o código refatorado!"
    This reverts commit f5a9fc6d13b1eaad3912460be204ea60ef22fba0.

--- index.js
+++
@@ -1,8 +0,0 @@
-const { buscarPostsReddit, removeFilesDuplicate } = require("./js/download_posts_reddit");
-
-function main(params) {
-    //removeFilesDuplicate();
-    buscarPostsReddit("wallpaper");
-}
-
-main();
