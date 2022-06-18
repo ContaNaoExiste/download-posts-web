f5a9fc6d13b1eaad3912460be204ea60ef22fba0
ContaNaoExiste <ContaNaoExiste@emaildebase0001@gmail.com>
2022-06-18 15:40:20 -0300

    Commit inicial com o código refatorado!

---
+++ index.js
@@ -0,0 +1,8 @@
+const { buscarPostsReddit, removeFilesDuplicate } = require("./js/download_posts_reddit");
+
+function main(params) {
+    //removeFilesDuplicate();
+    buscarPostsReddit("wallpaper");
+}
+
+main();
